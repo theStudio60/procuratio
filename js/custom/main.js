bee98db75e3217440ad8a5e59f7adf18baff12f5
Fabien Dupont <fab.d1987@yandex.com>
2022-08-10 10:27:57 +0200

    Update main.js

--- js/custom/main.js
+++ js/custom/main.js
@@ -16,8 +16,8 @@ AOS.init({
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 600, // values from 0 to 3000, with step 50ms
     easing: 'ease-in-out', // default easing for AOS animations
-    //once: true, // whether animation should happen only once - while scrolling down
-    mirror: true // whether elements should animate out while scrolling past them
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false // whether elements should animate out while scrolling past them
 
 });
 
