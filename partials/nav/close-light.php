13a1ff5ae3d95fabe4d375c530befe9aeb79aacf
Fabien Dupont <fab.d1987@yandex.com>
2022-08-10 10:27:30 +0200

    add close light and make burger white

---
+++ partials/nav/close-light.php
@@ -0,0 +1,13 @@
+
+<svg width="34px" height="34px" viewBox="0 0 34 34" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="mobile" stroke="none" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round">
+        <g id="Mobile" transform="translate(-271.000000, -23.000000)" stroke="#fff">
+            <g id="navbar" transform="translate(0.000000, 0.000000)">
+                <g id="close" transform="translate(272.000000, 24.000000)">
+                    <line x1="0" y1="32" x2="32" y2="0" id="Line"></line>
+                    <line x1="0" y1="-2.27373675e-12" x2="32" y2="32" id="Line"></line>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
