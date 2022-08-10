36fbfb954eaeb42b04d7d8ab3210897c9765c8ea
Fabien Dupont <fab.d1987@yandex.com>
2022-08-10 10:27:50 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -32,6 +32,8 @@ if( have_rows('acf_group_homepage') ):
     get_template_part( 'partials/sections/layout','control' );
     get_template_part( 'partials/sections/layout','services' );
     get_template_part( 'partials/sections/layout','values' );
+    get_template_part( 'partials/sections/layout','contact' );
+
   endwhile;
 endif;
 ?>
