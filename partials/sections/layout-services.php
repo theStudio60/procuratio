6a7f163e413df85821bc9f88723699b0a85c8785
Fabien Dupont <fab.d1987@yandex.com>
2022-08-17 11:29:16 +0200

    Update layout-services.php

--- partials/sections/layout-services.php
+++ partials/sections/layout-services.php
@@ -2,9 +2,9 @@
   while( have_rows('option-group_conseil','option') ): the_row();  
     if( have_rows('group_mandats') ): 
       while( have_rows('group_mandats') ): the_row(); 
-      $title = get_sub_field('title');
-      $description = get_sub_field('description'); 
-      $conclusion = get_sub_field('conclusion'); 
+        $title = get_sub_field('title');
+        $description = get_sub_field('description'); 
+        $conclusion = get_sub_field('conclusion'); 
       ?>
         <section id=" " class="section bg-white  py-3 py-lg-5"   data-aos="slide-up">
           <div class="container m-auto  services"    >
@@ -26,9 +26,7 @@
               <?php endif;?>
               <div class="col-12 col-lg-9">
                 <div class="container">
- 
                   <div class="row">
-
                     <?php
                     if( have_rows('repeater_mandats') ):
                       while( have_rows('repeater_mandats') ): the_row();
@@ -38,45 +36,41 @@
                             $price = get_sub_field('price');
                             $content = get_sub_field('wysiwyg'); 
                     ?>
-                        <div class="col-12 col-lg-6 col-xl-4 "     data-aos="flip-up" data-aos-duration="1000"   >
-                          <div class="card w-100 mx-0 mx-md-auto px-3 d-flex"   >
-                            <div class="hide-arc"></div>
-                            <div class="arc"></div>
-                            <?php if ($subtitle) :  ?>
-                              <span class="text-dark font-sans my-auto ml-auto w-75 ">
-                                <?= $subtitle ?>
-                              </span>
-                            <?php endif;?>
-                            <?php /*
-                            <?php if ($content) :  ?>
-                                <?= $content ?>
-                            <?php endif;?>
-                            <?php if ($price) :  ?>
-                              <div class="price">
-                                <?= $price ?>
-                              </div>
-                            <?php endif;?>
-                            //*/ ?>
-                          </div> <?php //end  card  ?>
-                        </div>
+                            <div class="col-12 col-lg-6 col-xl-4 "     data-aos="flip-up" data-aos-duration="1000"   >
+                              <div class="card w-100 mx-0 mx-md-auto px-3 d-flex"   >
+                                <div class="hide-arc"></div>
+                                <div class="arc"></div>
+                                <?php if ($subtitle) :  ?>
+                                  <span class="text-dark font-sans my-auto ml-auto w-75 ">
+                                    <?= $subtitle ?>
+                                  </span>
+                                <?php endif;?>
+                                <?php /*
+                                <?php if ($content) :  ?>
+                                    <?= $content ?>
+                                <?php endif;?>
+                                <?php if ($price) :  ?>
+                                  <div class="price">
+                                    <?= $price ?>
+                                  </div>
+                                <?php endif;?>
+                                //*/ ?>
+                              </div> <?php //end  card  ?>
+                            </div>
+                          <?php endwhile; ?>
+                        <?php endif; ?>
                       <?php endwhile; ?>
                     <?php endif; ?>
-                    <?php endwhile; ?>
-                    <?php endif; ?>
-                    <?php //end subgroup_services ?>
-      
-            </div>
-          </div>
-
-
-
-              <?php if ($conclusion) :  ?>
-                <div class="col-12 col-lg-9  ">
-                  <p>
-                    <?= $conclusion ?>
-                  </p>
+                  </div>
                 </div>
-              <?php endif;?>
+                <?php if ($conclusion) :  ?>
+                  <div class="w-100">
+                    <p>
+                      <?= $conclusion ?>
+                    </p>
+                  </div>
+                <?php endif;?>
+              </div>
             </div>
           </div>
         </section>
