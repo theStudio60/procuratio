a6f132853f6490b6bdeb75b3843d208b5d689f97
Fabien Dupont <fab.d1987@yandex.com>
2022-08-21 18:37:03 +0200

    Update layout-services.php

--- partials/sections/layout-services.php
+++ partials/sections/layout-services.php
@@ -7,8 +7,8 @@
         $conclusion = get_sub_field('conclusion');
         $image = get_sub_field('image');
       ?>
-        <section id=" " class="section bg-white h-25 py-3 py-lg-5"   data-aos="slide-up">
-          <div class="container m-auto  services"    >
+        <section id=" " class="section bg-white h-25 py-3 py-lg-5" >
+          <div class="container m-auto  services"    data-aos="slide-up"  >
             <div class="row">
               <div class="col-12">
                 <?php if ($title) :  ?>
