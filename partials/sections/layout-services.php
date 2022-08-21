a10615b4ef60885908ee0045990fcf0295a86cf8
Fabien Dupont <fab.d1987@yandex.com>
2022-08-21 18:02:40 +0200

    Update layout-services.php

--- partials/sections/layout-services.php
+++ partials/sections/layout-services.php
@@ -7,95 +7,48 @@
         $conclusion = get_sub_field('conclusion');
         $image = get_sub_field('image');
       ?>
-        <section id=" " class="section bg-white  py-3 py-lg-5"   data-aos="slide-up">
+        <section id=" " class="section bg-white h-25 py-3 py-lg-5"   data-aos="slide-up">
           <div class="container m-auto  services"    >
             <div class="row">
-            <div class="col-12 col-lg-8">
+              <div class="col-12 col-lg-8">
                 <?php if ($title) :  ?>
-
                   <h3 class="text-dark d-flex mb-3" >
                     <div class="dash mr-2 ml-n4 my-auto"></div>
                     <?= $title ?>
                   </h3>   
-     
                 <?php endif;?>
-                <?php if ($description) :  ?>
-               
+                <?php if ($description) :  
+                  $this_excerpt = wp_trim_words( $description, 35, ' ...  '); 
+                ?>
                   <p>
-                    <?= $description ?>
+                    <?= $this_excerpt ?>
                   </p>
-       
                 <?php endif;?>
-      
-                <div class="container">
-                  <div class="row">
-                    <?php
-                    if( have_rows('repeater_mandats') ):
-                      while( have_rows('repeater_mandats') ): the_row();
-                        if( have_rows('mandat') ):
-                          while( have_rows('mandat') ): the_row();
-                            $subtitle = get_sub_field('service');
-                            $price = get_sub_field('price');
-                            $content = get_sub_field('wysiwyg'); 
-                    ?>
-                            <div class="col-12 col-lg-6 col-xl-4">
-                              <div class="card w-100 mx-0 mx-md-auto px-3 d-flex"   >
-                                <div class="hide-arc"></div>
-                                <div class="arc"></div>
-                                <?php if ($subtitle) :  ?>
-                                  <span class="text-dark font-sans my-auto ml-auto w-75 ">
-                                    <?= $subtitle ?>
-                                  </span>
-                                <?php endif;?>
-                                <?php /*
-                                <?php if ($content) :  ?>
-                                    <?= $content ?>
-                                <?php endif;?>
-                                <?php if ($price) :  ?>
-                                  <div class="price">
-                                    <?= $price ?>
-                                  </div>
-                                <?php endif;?>
-                                //*/ ?>
-                              </div> <?php //end  card  ?>
-                            </div>
-                          <?php endwhile; ?>
-                        <?php endif; ?>
-                      <?php endwhile; ?>
-                    <?php endif; ?>
-                  </div>
-                </div>
-                <?php if ($conclusion) :  ?>
-                  <div class="w-100">
-                    <p>
-                      <?= $conclusion ?>
-                    </p>
-                  </div>
-                <?php endif;?>
-
               </div>
-                      
               <div
                 data-aos="flip-right" 
-                class="col-lg-4 d-none d-lg-flex h-100 my-auto"
+                class="col-lg-4 d-none d-lg-flex h-100 my-auto bg-primary"
                 style="background: url('<?php echo $image['url']; ?>');
                 background-repeat:no-repeat;
                 background-size:cover;
                 background-position:center ;
-                min-height:100vh;
+                min-height:260px;
                 "
               >
               </div>
               <div
                 data-aos="flip-up" 
-                class="col-12 d-block d-lg-none"
+                class="col-12 d-block d-lg-none bg-primary"
                 style="background: url('<?php echo $image['url']; ?>');
                 background-repeat:no-repeat;
                 background-size:cover;
                 background-position:center ;
-                min-height:380px;
+                min-height:180px;
                 "
               >
+              </div>
+              <div class="row w-100 d-flex mt-5">
+                  <button  onclick="location.href='<?php echo site_url(); ?>/fiduciaire#mandats'" class="m-auto button">En savoir plus</button>
               </div>
             </div>
           </div>
