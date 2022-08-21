217a44076bf6229b638586dccee4b4f7f562c741
Fabien Dupont <fab.d1987@yandex.com>
2022-08-21 16:25:32 +0200

    Update conseil-page.php

--- conseil-page.php
+++ conseil-page.php
@@ -71,18 +71,18 @@ endif;
       $subtitle = get_sub_field('subtitle');
       $content = get_sub_field('content'); 
       ?>
-        <section id="control" class="section d-flex   py-3 py-lg-5 bg-dark" >
-            <div class="container m-auto   " data-aos="fade-up" data-aos-mirror="false" >
-              <div class="row my-3  ">
-                <div class="col-12 col-lg-9 mx-auto mx-lg-0 ">
-                  <?php if ($title) :  ?>
-                    <div class="d-flex w-100 ">
-                      <div class="dash mr-2 ml-n4 my-auto"></div>
-                      <h3 class="text-white d-flex mb-3" >
-                          <?= $title ?>
-                      </h3>  
-                    </div>
-                  <?php endif;?>
+      <section id="control" class="section d-flex   py-3 py-lg-5 bg-dark"  >
+          <div class="container m-auto   " data-aos="fade-up" data-aos-mirror="false"  >
+            <div class="row my-3  ">
+              <div class="col-12 col-lg-9 mx-auto mx-lg-0 ">
+                <?php if ($title) :  ?>
+                  <div class="d-flex w-100 ">
+                    <div class="dash mr-2 ml-n4 my-auto"></div>
+                    <h3 class="text-white d-flex mb-3" >
+                        <?= $title ?>
+                    </h3>  
+                  </div>
+                <?php endif;?>
                   <?php if ($content) :  ?>
                     <div class="text-white d-block d-lg-none px-2"  >
                         <?= $content?>
@@ -99,43 +99,44 @@ endif;
                         color:#fff;
                       }
                       #summary div.collapse:not(.show) {
-                    height:64px;
-                    overflow: hidden;
-                    display: -webkit-box;
-                    -webkit-line-clamp: 3;
-                    -webkit-box-orient: vertical;  
-                      /* Safari 10.1 */
-                      @media not all and (min-resolution:.001dpcm){ 
-                      @supports (-webkit-appearance:none) and (not (stroke-color:transparent)) {
-                      
-                      height:auto; 
-                      
-                      }}
- 
-                }
-
-                #summary div.collapsing {
-                    min-height: 192px;
-                }
-                #summary a.reduction.collapsed:after  {
-                    content: 'Lire la suite'; 
-                    width: 100%;
-                    font-size:18px; 
-                    margin-left:auto ; margin-right:auto;
-                    text-align:center;
-                    font-weight:600;
-                    font-family:"Raleway";
-                }
-
-                #summary a.reduction:not(.collapsed):after {
-                    content: '- Réduire';
-                    font-size:18px;
-                    padding-top:16px;
-                    margin-left:auto ; margin-right:auto;
-                    text-align:center;
-                    font-weight:600;
-                    font-family:"Raleway";
-                }
+                          height:64px;
+                          overflow: hidden;
+                          display: -webkit-box;
+                          -webkit-line-clamp: 3;
+                          -webkit-box-orient: vertical;  
+                            /* Safari 10.1 */
+                            @media not all and (min-resolution:.001dpcm){ 
+                            @supports (-webkit-appearance:none) and (not (stroke-color:transparent)) {
+                            
+                            height:auto; 
+                            
+                            }}
+                          
+                      }
+                    
+                      #summary div.collapsing {
+                          min-height: 192px;
+                      }
+                      #summary a.reduction.collapsed:after  {
+                          content: 'Lire la suite'; 
+                          width: 100%;
+                          font-size:18px; 
+                          margin-left:auto ; margin-right:auto;
+                          text-align:center;
+                          font-weight:600;
+                          font-family:"Raleway";
+                      }
+                    
+                      #summary a.reduction:not(.collapsed):after {
+                          content: '- Réduire';
+                          font-size:18px;
+                          padding-top:16px;
+                          margin-left:auto ; margin-right:auto;
+                          text-align:center;
+                          font-weight:600;
+                          font-family:"Raleway";
+                      }
+                    
                       .wrapper-c {
                           position: relative;
                           margin: 40px auto;
@@ -214,24 +215,26 @@ endif;
                           }
                       }
                       </style>
+
                       <div class="d-none d-lg-block" id="summary">
                         <div class="collapse mb-2" id="collapseSummary">
                           <?= $content?>
                         </div>
-                        <a class="collapsed text-secondary" data-toggle="collapse" href="#collapseSummary" aria-expanded="false" aria-controls="collapseSummary"></a>
+                        <a class="collapsed text-secondary reduction" data-toggle="collapse" href="#collapseSummary" aria-expanded="false" aria-controls="collapseSummary"></a>
                       </div>
+                    
                     </div>   
                   <?php endif;?>
-                </div> 
-                <div class="col-12 col-lg-3 d-flex"> 
-                  <div class="wrapper-c">
-                    <div class="pie spinner"></div>
-                    <div class="pie filler"></div>
-                    <div class="mask"></div>
-                  </div>
+              </div> 
+              <div class="col-12 col-lg-3 d-flex"> 
+                <div class="wrapper-c">
+                  <div class="pie spinner"></div>
+                  <div class="pie filler"></div>
+                  <div class="mask"></div>
                 </div>
               </div>
             </div>
+          </div>
         </section>
       <?php endwhile; ?>
       <?php wp_reset_postdata(  ) ;?>
