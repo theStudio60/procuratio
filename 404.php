bd5b6312396eb219b59ef4e1412ecbd885ab8431
francoismg <francois.moriergenoud@gmail.com>
2022-08-23 15:15:36 +0200

    test commit

--- 404.php
+++ 404.php
@@ -5,11 +5,12 @@
       <?php while( have_rows('404_content_group') ): the_row();
         $content = get_sub_field('wysiwyg');
       ?>
-        <section class="section mt-5" style="min-height:80vh"> 
+        <section class="section mt-5" style="min-height:80vh">
           <div class=" section__container">
             <div class="section__row">
               <div class="section__col mt-5">
                 <?= $content ?>
+				  
               </div>
             </div>
           </div>
