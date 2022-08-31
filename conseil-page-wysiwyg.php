cbcb24cf2fce0c16de8f20bd460c63c4c6b15f70
francoismg <francois.moriergenoud@gmail.com>
2022-08-31 16:10:07 +0200

    commit

--- conseil-page-wysiwyg.php
+++ conseil-page-wysiwyg.php
@@ -5,17 +5,20 @@
  * @package studio_soixante
  */
 //get_header();
-/*if( have_rows('page-conseil_acf_group_wysiwyg') ):
+
+if( have_rows('page-conseil_acf_group_wysiwyg') ):
   while( have_rows('page-conseil_acf_group_wysiwyg') ): the_row();
     get_template_part( 'partials/sections/hero', 'page' );
   endwhile;
-endif;*/
+endif;
 
+/*
 if( have_rows('page-conseil_acf_group') ):
 	while( have_rows('page-conseil_acf_group') ): the_row();
 		get_template_part( 'partials/sections/hero', 'page' );
 	endwhile;
 endif;
+*/
 
 ?>
 <style>body.page-wrapper{background-color:#002369!important;}</style>
