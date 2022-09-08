c77b13048a791640a202aac8c9c55828bc2f2053
Fabien Dupont <fab.d1987@yandex.com>
2022-09-08 12:42:25 +0200

    update

--- js/custom/main.js
+++ js/custom/main.js
@@ -11,9 +11,9 @@ if (isTouch) {
 
 AOS.init({
     disable: 'mobile',
-    debounceDelay: 1500,
+//    debounceDelay: 1500,
     // offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
+    delay: 400, // values from 0 to 3000, with step 50ms
     duration: 800, // values from 0 to 3000, with step 50ms
     easing: 'ease-in-out', // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down
