4589abf067cb37781b1cb1619a43919d2aa850b4
Fabien Dupont <fab.d1987@yandex.com>
2022-09-09 16:15:32 +0200

    Update layout-presentation.php

--- partials/sections/layout-presentation.php
+++ partials/sections/layout-presentation.php
@@ -15,7 +15,7 @@ if( have_rows('group_presentation') ):
 ?>
   <section  
     id="presentation"
-    class=" bg-dark" 
+    class=" bg-white" 
   >  
    
     <div class="container  py-3 py-lg-5 ">
@@ -23,14 +23,14 @@ if( have_rows('group_presentation') ):
         <div class="col-12 col-lg-6 p-4 p-lg-2" id="quisommesnous" data-aos="slide-right" data-aos-duration="800"  >
           <div class="section-text m-auto">
             <?php if ($title) :  ?>
-              <h3 class="text-white d-flex mb-3" >
+              <h3 class="text-dark d-flex mb-3" >
                 <div class="dash mr-2 ml-n4 my-auto"></div>
                 <?= $title ?>
               </h3>  
             <?php endif;?>
  
             <?php if ($content) :  ?>
-              <div class="mt-2 div-white text-white"  >
+              <div class="mt-2"  >
                 <?= $content?>
 
               </div>
