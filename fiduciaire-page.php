237ac0f598e65cc8f9414c3b1160cd34780157e2
Fabien Dupont <fab.d1987@yandex.com>
2022-09-09 15:37:41 +0200

    Update fiduciaire-page.php

--- fiduciaire-page.php
+++ fiduciaire-page.php
@@ -376,7 +376,7 @@ if( have_rows('page-fiduciaire_acf_group') ):
                                 </span>
                               <?php endif;?> 
                               <?php if ($name) :  ?>
-                                <span class="text-center text-dark font-sans m-auto mt-2 mb-auto">
+                                <span class="text-center text-white font-sans m-auto mt-2 mb-auto">
                                   <?= $name ?>
                                 </span>
                               <?php endif;?> 
