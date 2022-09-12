3eba3266ee093b474bb57394e1c77b2cea902487
Fabien Dupont <fab.d1987@yandex.com>
2022-09-12 20:40:17 +0200

    Create layout-conseil.php

---
+++ partials/sections/layout-conseil.php
@@ -0,0 +1,68 @@
+<?php if( have_rows('group_conseil') ):
+      while( have_rows('group_conseil') ): the_row();
+        $title = get_sub_field('title');
+        $description = get_sub_field('wysiwyg');
+        $conclusion = get_sub_field('conclusion');
+        //$image = get_sub_field('image');
+    ?>
+        <section id="conseil" class="section bg-light  py-3 py-lg-5">
+          <div class="container m-auto services"  data-aos="fade-up" data-aos-duration="800">
+            <div class="row">
+              <div class="col-12 col-lg-9 mx-auto">
+                <?php if ($title) :  ?>
+                  <div class="mx-auto d-flex w-100">
+                    <h3 class="text-dark d-flex mb-3 mx-auto text-center" >
+                      <div class="dash mr-2 ml-n4 my-auto"></div>
+                      <?= $title ?>
+                    </h3>
+                  </div>
+
+                <?php endif;?>
+                <?php if ($description) :  ?>
+                  <div class="div-centered">
+                    <?= $description ?>
+                  </div>
+                <?php endif;?>
+                <div class="container mx-auto">
+                  <div class="row">
+                    <?php 
+                    if( have_rows('repeater_services') ):
+                      while( have_rows('repeater_services') ): the_row();
+                        if( have_rows('service') ):
+                          while( have_rows('service') ): the_row();
+                            $name = get_sub_field('name'); 
+                            $picto = get_sub_field('picto'); 
+                    ?>
+                            <div class="col-12 col-md-6 col-lg-4 d-flex">
+                              <?php if ($picto) :  ?>
+                                <span class=" mt-auto mx-auto d-flex">
+                                  <img class="mx-auto" width="64px" height="64px" src="<?php echo $picto['url']; ?>" alt="">
+                                </span>
+                              <?php endif;?> 
+                              <?php if ($name) :  ?>
+                                <span class="text-center text-dark font-sans m-auto mt-2 mb-auto">
+                                  <?= $name ?>
+                                </span>
+                              <?php endif;?> 
+                            </div>
+                          <?php endwhile; ?>
+                        <?php endif; ?>
+                      <?php endwhile; ?>
+                    <?php endif; ?>
+                  </div>
+                </div>
+                <?php if ($conclusion) :  ?>
+                  <div class="div-centered">
+                    <p>
+                      <?= $conclusion ?>
+                    </p>
+                  </div>
+                <?php endif;?>
+              </div>
+              
+            </div>
+          </div>
+        </section>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(  ) ;?>
+    <?php endif; ?>
