895efd2e4a713c03d15d5a8da500caad5e8c4061
Fabien Dupont <fab.d1987@yandex.com>
2022-09-12 19:54:50 +0200

    Update layout-control.php

--- partials/sections/layout-control.php
+++ partials/sections/layout-control.php
@@ -22,6 +22,7 @@
             </div>
           <?php endif;?>
           <style>
+            /*
           #summary { line-height: 1.5; }
           .text-white p {
             font-size: 18px;
@@ -32,19 +33,19 @@
               margin: 40px auto;
               background: rgba(255,255,255,0);
           }
-
+        
           .wrapper-c,
           .wrapper-c * {
               -moz-box-sizing: border-box;
               -webkit-box-sizing: border-box;
               box-sizing: border-box;
           }
-
+        
           .wrapper-c {
               width: 196px;
               height: 196px;
           }
-
+        
           .wrapper-c .pie {
               width: 50%;
               height: 100%;
@@ -53,20 +54,20 @@
               background: rgba(255,255,255,1);
               border: 5px solid #29FFFF;
           }
-
+        
           .wrapper-c .spinner {
               border-radius: 100% 0 0 100% / 50% 0 0 50%;
               z-index: 200;
               border-right: none;
               animation: rota 5s linear infinite;
           }
-
+        
           .wrapper-c:hover .spinner,
           .wrapper-c:hover .filler,
           .wrapper-c:hover .mask {
               animation-play-state: running;
           }
-
+        
           .wrapper-c .filler {
               border-radius: 0 100% 100% 0 / 0 50% 50% 0;
               left: 50%;
@@ -75,7 +76,7 @@
               animation: opa 5s steps(1, end) infinite reverse;
               border-left: none;
           }
-
+        
           .wrapper-c .mask {
               width: 50%;
               height: 100%;
@@ -85,7 +86,7 @@
               z-index: 300;
               animation: opa 5s steps(1, end) infinite;
           }
-
+        
           @keyframes rota {
               0% {
                   transform: rotate(0deg);
@@ -94,7 +95,7 @@
                   transform: rotate(360deg);
               }
           }
-
+        
           @keyframes opa {
               0% {
                   opacity: 1;
@@ -104,6 +105,7 @@
                   opacity: 0;
               }
           }
+          //*/
           </style>
           <?php if ($content) :
             $this_excerpt = wp_trim_words( $content, 50, ' ...  ');
@@ -116,13 +118,13 @@
 
           <?php endif;?>
         </div>
-        <div class="col-12 col-lg-3 d-flex">
+        <div class="col-12 col-lg-3 py-2 d-flex">
           <!--div class="wrapper-c">
             <div class="pie spinner"></div>
             <div class="pie filler"></div>
             <div class="mask"></div>
           </div-->
-			<img src="<?php echo $piechart ?>" />
+			    <img height="256px" class="mx-auto" src="<?php echo $piechart ?>" />
         </div>
       </div>
     </div>
