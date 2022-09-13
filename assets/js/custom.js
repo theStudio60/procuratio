0bb908acac186866d21a9cba8edb203553864df7
Fabien Dupont <fab.d1987@yandex.com>
2022-09-13 20:14:09 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,12 +10,22 @@ if (isTouch) {
 ; //AOS.init();
 
 AOS.init({
+
     disable: 'mobile',
-    debounceDelay: 500,
-    // the delay on debounce used while resizing window (advanced)
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+
+    debounceDelay: 400,
+
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    // once: true, // whether animation should happen only once - while scrolling down
-    mirror: false // whether elements should animate out while scrolling past them
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+
 
 });
 
