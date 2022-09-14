211efc1e0762c6c928a65750601a6032c916b84a
Fabien Dupont <fab.d1987@yandex.com>
2022-09-14 16:42:16 +0200

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> style="scroll-behavior: smooth;">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <?php wp_head(); ?>
