148cc9a44d6b1054d287f64c37a515623ca2e941
Fabien Dupont <fab.d1987@yandex.com>
2022-09-15 16:48:22 +0200

    Update layout-control.php

--- partials/sections/layout-control.php
+++ partials/sections/layout-control.php
@@ -23,119 +23,115 @@
             </div>
           <?php endif;?>
           <style>
-              /*
-            #summary { line-height: 1.5; }
-            .text-white p {
-              font-size: 18px;
-              color:#fff;
-            }
-            .wrapper-c {
-                position: relative;
-                margin: 40px auto;
-                background: rgba(255,255,255,0);
-            }
-
-            .wrapper-c,
-            .wrapper-c * {
-                -moz-box-sizing: border-box;
-                -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-            }
-
-            .wrapper-c {
-                width: 196px;
-                height: 196px;
-            }
-
-            .wrapper-c .pie {
-                width: 50%;
-                height: 100%;
-                transform-origin: 100% 50%;
-                position: absolute;
-                background: rgba(255,255,255,1);
-                border: 5px solid #29FFFF;
-            }
-
-            .wrapper-c .spinner {
-                border-radius: 100% 0 0 100% / 50% 0 0 50%;
-                z-index: 200;
-                border-right: none;
-                animation: rota 5s linear infinite;
-            }
-
-            .wrapper-c:hover .spinner,
-            .wrapper-c:hover .filler,
-            .wrapper-c:hover .mask {
-                animation-play-state: running;
-            }
-
-            .wrapper-c .filler {
-                border-radius: 0 100% 100% 0 / 0 50% 50% 0;
-                left: 50%;
-                opacity: 0;
-                z-index: 100;
-                animation: opa 5s steps(1, end) infinite reverse;
-                border-left: none;
-            }
-
-            .wrapper-c .mask {
-                width: 50%;
-                height: 100%;
-                position: absolute;
-                background: inherit;
-                opacity: 1;
-                z-index: 300;
-                animation: opa 5s steps(1, end) infinite;
-            }
-
-            @keyframes rota {
-                0% {
-                    transform: rotate(0deg);
-                }
-                100% {
-                    transform: rotate(360deg);
-                }
-            }
-
-            @keyframes opa {
-                0% {
-                    opacity: 1;
-                }
-                50%,
-                100% {
-                    opacity: 0;
-                }
-            }
-            //*/
+            /*
+          #summary { line-height: 1.5; }
+          .text-white p {
+            font-size: 18px;
+            color:#fff;
+          }
+          .wrapper-c {
+              position: relative;
+              margin: 40px auto;
+              background: rgba(255,255,255,0);
+          }
+        
+          .wrapper-c,
+          .wrapper-c * {
+              -moz-box-sizing: border-box;
+              -webkit-box-sizing: border-box;
+              box-sizing: border-box;
+          }
+        
+          .wrapper-c {
+              width: 196px;
+              height: 196px;
+          }
+        
+          .wrapper-c .pie {
+              width: 50%;
+              height: 100%;
+              transform-origin: 100% 50%;
+              position: absolute;
+              background: rgba(255,255,255,1);
+              border: 5px solid #29FFFF;
+          }
+        
+          .wrapper-c .spinner {
+              border-radius: 100% 0 0 100% / 50% 0 0 50%;
+              z-index: 200;
+              border-right: none;
+              animation: rota 5s linear infinite;
+          }
+        
+          .wrapper-c:hover .spinner,
+          .wrapper-c:hover .filler,
+          .wrapper-c:hover .mask {
+              animation-play-state: running;
+          }
+        
+          .wrapper-c .filler {
+              border-radius: 0 100% 100% 0 / 0 50% 50% 0;
+              left: 50%;
+              opacity: 0;
+              z-index: 100;
+              animation: opa 5s steps(1, end) infinite reverse;
+              border-left: none;
+          }
+        
+          .wrapper-c .mask {
+              width: 50%;
+              height: 100%;
+              position: absolute;
+              background: inherit;
+              opacity: 1;
+              z-index: 300;
+              animation: opa 5s steps(1, end) infinite;
+          }
+        
+          @keyframes rota {
+              0% {
+                  transform: rotate(0deg);
+              }
+              100% {
+                  transform: rotate(360deg);
+              }
+          }
+        
+          @keyframes opa {
+              0% {
+                  opacity: 1;
+              }
+              50%,
+              100% {
+                  opacity: 0;
+              }
+          }
+          //*/
           </style>
-
-          <div class="text-white d-block font-serif w-100 lead mb-3 "  >
-            <?php if ($content) :
-              $this_excerpt = wp_trim_words( $content, 50, ' ...  ');
-            ?>
+          <?php if ($content) :
+            $this_excerpt = wp_trim_words( $content, 50, ' ...  ');
+          ?>
+            <div class="text-white d-block font-serif w-100 lead mb-3 "  >
               <?= $this_excerpt ?>
-            <?php endif;?>
-            <?php $thisLink = get_sub_field('link'); // var_dump( $thisLink ); ?>
-            <?php if ($thisLink) :
-              $link_url = $thisLink['url'];
-              $link_title = $thisLink['title'];
-              $link_target = $thisLink['target'] ? $thisLink['target'] : '_self';
-            ?>
- 
-              <a class="text-secondary" href="<?php echo link_url(); ?>">
-                <?= $link_title ?>
-              </a>
-            <?php endif; ?>
-          </div>
-
+              <br>
+              <a class="text-secondary" href="<?php echo site_url(); ?>/conseil#control">Lire la suite</a>
+            </div>
+            <?php /*
+            <button class="button" onclick="location.href='<?php echo site_url(); ?>/conseil#control'">En savoir plus</button>
+            //*/ ?>
+          <?php endif;?>
         </div>
-        <div class="col-12 col-lg-3 py-2 d-flex"> 
+        <div class="col-12 col-lg-3 py-2 d-flex">
+          <!--div class="wrapper-c">
+            <div class="pie spinner"></div>
+            <div class="pie filler"></div>
+            <div class="mask"></div>
+          </div-->
 			    <img height="256px" class="mx-auto" src="<?php echo $piechart ?>" />
         </div>
       </div>
     </div>
 </section>
-
 <?php endwhile; ?>
 <?php endif; ?>
 <?php endwhile; ?>
