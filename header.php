b1db50c885c57cfda5ae78c86a5fd123bdf1de29
Fabien Dupont <fab.d1987@yandex.com>
2022-09-15 13:53:07 +0200

    Update header.php

--- header.php
+++ header.php
@@ -100,7 +100,7 @@ $(document).ready(function() {
     </script>
     <div class="container-fluid   d-flex  my-auto">
         <a class="my-auto " href="<?= get_home_url() ?>">
-            <img width="auto"  class="" id="headerBrand" style="height:72px;margin-left:16px;transition:400ms" src="<?= $logotype_w ?>" alt="<?= get_bloginfo() ?>">
+            <img width="auto"  class="ml-2" id="headerBrand" style="height:72px;margin-left:16px;transition:400ms" src="<?= $logotype_w ?>" alt="<?= get_bloginfo() ?>">
         </a>
         <?php
           wp_nav_menu( array(
@@ -141,7 +141,7 @@ $(document).ready(function() {
                 type="button"
                 style=""
                 onclick="location.href='<?php echo $link_url ?>'"
-                class="button"
+                class="button mr-2"
                 >
                     <?= $link_title ?>
                 </button>
