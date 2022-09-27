b73b4458d41c936cc6219b477ac80cd35d8d795a
Fabien Dupont <fab.d1987@yandex.com>
2022-09-27 16:17:48 +0200

    Update hero-page-new.php

--- partials/sections/hero-page-new.php
+++ partials/sections/hero-page-new.php
@@ -11,13 +11,11 @@ endif;
 //*
 if ( has_post_thumbnail() ) { 
   $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-  } ; 
+} ; 
 //*/  
 ?>
-<section class="hero" style="background: url(' ');"> 
-
-
-  <div class="hero--page__overlay hero--page__overlay--default h-100">
+<section class="hero" style="background: url('<?php echo $backgroundImg[0];?>');"> 
+  <div class="hero__overlay h-100">
     <div class="container p-4 w-100" >
       <div class="row h-100">
         <div class="col-12 col-lg-8 h-100 d-flex p-0" >
@@ -26,10 +24,10 @@ if ( has_post_thumbnail() ) {
                 <?php the_title(); ?> 
               </h1>
               <?php if ($content) :  ?>
-                  <style > .hero-content p{color:white!important;}</style>
-                  <div class="div-left div-white my-2"> 
-                    <?= $content?>
-                  </div>
+                <style > .hero-content p{color:white!important;}</style>
+                <div class="div-left div-white my-2"> 
+                  <?= $content?>
+                </div>
               <?php endif;?>
               <?php if ($thisLink) :
                 $link_url = $thisLink['url'];
@@ -38,43 +36,75 @@ if ( has_post_thumbnail() ) {
               ?>
                 <div class="w-100 d-md-flex  text-center">
                   <button
-                  type="button" 
-                  style="" 
-                  onclick="location.href='<?php echo $link_url ?>'"
-                  class="button" 
+                   type="button" 
+                   style="" 
+                   onclick="location.href='<?php echo $link_url ?>'"
+                   class="button" 
                   > 
-                      <?= $link_title ?>
+                    <?= $link_title ?>
                   </button>  
                 </div> 
               <?php endif;?>   
             </div>
-        
         </div>
       </div>
     </div> 
-
-
   </div>
+  <?php  
+  if( have_rows('group_hero') ): 
+    while( have_rows('group_hero') ): the_row();
+      if( have_rows('group_slider') ): 
+        while( have_rows('group_slider') ): the_row();?>
+          <div class="hero__slider slick-slider">
 
+            <?php if( have_rows('repeater') ): ?>
+              <?php while( have_rows('repeater') ): the_row();  
+ 
+                          $img = get_sub_field('image');
 
-
-  <div class="hero__slider slick-slider">
-    <div class="hero__slider__img" style="background: url('<?php echo $backgroundImg[0]; ?>');">
+                        ?>
+       <?php if ($img) :  ?>
+              <div class="hero__slider__img" style="background-size:cover;background-repeat:no-repeat;background-position:center;
+    background: url('<?php echo $img['url']; ?>');">
 
     </div>
-    <div class="hero__slider__img" style="background: url('<?php echo $backgroundImg[0]; ?>');">
+<?php endif; ?>
 
-</div>
-<div class="hero__slider__img" style="background: url('<?php echo $backgroundImg[0]; ?>');">
+    <?php
+            endwhile;
+          endif;
+          ?>
+          </div>
 
-</div>
+        <?php
+        endwhile;
+      endif;
+    endwhile;
+  endif;
+  ?>  
+
+  <div class="hero__slider slick-slider">
+
+
+
+
+    <div class="hero__slider__img" style="background-size:cover;background-repeat:no-repeat;background-position:center;
+    background: url('<?php echo $backgroundImg[0]; ?>');">
+
+    </div>
+    <div class="hero__slider__img" style="background-size:cover;background-repeat:no-repeat;background-position:center;
+    background: url('<?php echo $backgroundImg[0]; ?>');">
+
+  </div>
+  
+
   </div>
 
 <style>
 
 .hero__overlay{
   position:absolute;
-  height: 100vh;
+  height: 80vh;
       //calc(100vh - 36px) !important;;
   width:100vw!important;z-index:20!important; 
     background: -webkit-gradient(linear, left bottom, right top, from(rgba(0, 35, 105, 0.8)), to(rgba(0, 35, 105, 0.99))) !important;
@@ -86,14 +116,14 @@ if ( has_post_thumbnail() ) {
     padding: 0 !important;
     position: absolute;
     width: 100vw;
-    height: 100vh;
+    height: 80vh;
       //calc(100vh - 36px) !important;
     z-index: 1;
 }
 
 
 .hero__slider__img {
-    height: 100vh;
+    height: 80vh;
       //calc(100vh - 36px) !important;
     background-position: center;
     background-repeat: no-repeat;
